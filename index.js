51dd9c2275f104bdc8165d9db3fe66a7f304798c
Supriya Haldar <112514582+Supriya9002@users.noreply.github.com>
2024-07-31 12:51:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ import cors from 'cors';
 import path from 'path';
 
 dotenv.config()
-// mongoose
-//     .connect(process.env.MONGO)
-//     .then(()=>{
-//         console.log('Connected to MongoDB');
-//     })
-//     .catch((err) => {
-//     console.log(err);
-// });
-
 
 const connectToDatabase = async () => {
     try {
@@ -39,16 +30,12 @@ const connectToDatabase = async () => {
 
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-// app.listen(3000,()=>{
-//         console.log('Server is running on port 3000!')
-//     }
-// );
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}!`);
